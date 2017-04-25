f9a85f0197c914aa09088ea02fad3c2261e65881
halle tempest and phat nguyen <phat@>
2017-04-25 15:31:43 -0700

    Created Flashcard-site

---
+++ JS/scripts.js
@@ -0,0 +1,13 @@
+$(function() {
+  $(".clickable").click(function() {
+    $("#jss").toggle();
+    $("#jsh").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable2").click(function() {
+    $("#Fs").toggle();
+    $("#Fh").toggle();
+  });
+});
